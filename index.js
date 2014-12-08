76867e76fa5618eee1f81cdd68d89b2ef8f7545b
imGqb <iamgqb@gmail.com>
2014-12-08 17:18:01 +0800

    resolve export & TODO

--- index.js
+++ index.js
@@ -3,23 +3,22 @@
  * Created by Gqb on 14/11/9.
  */
 
-var PSD = require('./lib/init')('./blank.psd');
-
 var parseHeader = require('./lib/header'),
     parseColorMode = require('./lib/colorMode'),
     parseImageResources = require('./lib/imageResources'),
-    parseLayerMaskInfo = require('./lib/layerMaskInfo');
+    parseLayerMaskInfo = require('./lib/layerMaskInfo'),
+    parseImageData = require('./lib/imageData'),
+    init = require('./lib/init');
 
-parseHeader(PSD);
-parseColorMode(PSD);
-parseImageResources(PSD);
-parseLayerMaskInfo(PSD);
-//parseImage(PSD);
-
-
-//console.log(PSD.layerMaskInfo.layerInfo.layers);
-//PSD.layerMaskInfo.layerInfo.layers[0].saveAsPng('test.png')
-
-module.exports = function(path){
-    return require('./lib/init')(path)
+exports.parse = function(path){
+    var PSD = init(path);
+    parseHeader(PSD);
+    parseColorMode(PSD);
+    parseImageResources(PSD);
+    parseLayerMaskInfo(PSD);
+    parseImageData(PSD);
+    return PSD
 };
+//TODO 树形分层
+//一些get方法，如链接图层
+//文字图层的相应处理
