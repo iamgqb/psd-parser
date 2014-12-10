0e4433d0111684204b11c8238587fe35502706eb
越澄 <qingbo.gqb@alibaba-inc.com>
2014-12-10 16:53:18 +0800

    test case

--- example/test.js
+++ example/test.js
@@ -4,15 +4,15 @@
 
 var PSD = require('../index');
 
-var psd = PSD.parse('./example/blank.psd');
+var psd = PSD.parse('./example/1024.psd');
 //console.log(PSD.layerMaskInfo.layerInfo.layers);
 //PSD.layerMaskInfo.layerInfo.layers[0].saveAsPng('test.png')
 //console.log(psd.imageData);
 //psd.imageData.saveAsPng('./example/image/test.png');
 console.time('go')
-console.log(psd.file.buffer.length)
+//console.log(psd.file.buffer.length)
 //psd.imageData.saveAsPng('./example/test.png')
-console.log(psd)
+console.log(psd.getTree())
 //for(var i=0;i<psd.layerMaskInfo.layerInfo.layers.length;i++){
 //    psd.layerMaskInfo.layerInfo.layers[i].saveAsPng('./example/'+i+'.png')
 //}
